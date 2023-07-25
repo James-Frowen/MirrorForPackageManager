0eeedc808637fcd05bab014b6ae5618e9f80422c
James Frowen <jamesfrowendev@gmail.com>
2023-07-25 15:37:29 +0100

    force push tags

--- PrepareForPackageManager.sh
+++ PrepareForPackageManager.sh
@@ -58,4 +58,4 @@ git add .
 git commit -m "CI: adding package.json"
 git push -f 
 # push all tags, this needs to be done here because it causes the release step to fail
-git push --tags HEAD:master
+git push -f --tags HEAD:master
